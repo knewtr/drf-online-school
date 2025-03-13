249d67768e862f94b3c7eb752502c3448b7ae9dd
Nataly Veldina <221fuchs@gmail.com>
2025-03-13 21:15:45 +0500

    add models and serializers

---
+++ materials/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name="Название курса", help_text="Укажите название курса"
+    )
+    description = models.TextField(
+        verbose_name="Описание курса",
+        blank=True,
+        null=True,
+        help_text="Укажите описание курса",
+    )
+    preview = models.ImageField(
+        upload_to="materials/preview", blank=True, null=True, verbose_name="Превью"
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Укажите название урока",
+    )
+    description = models.TextField(
+        verbose_name="Описание урока",
+        blank=True,
+        null=True,
+        help_text="Укажите описание урока",
+    )
+    preview = models.ImageField(
+        upload_to="materials/preview", blank=True, null=True, verbose_name="Превью"
+    )
+    link = models.URLField(
+        verbose_name="Ссылка на урок",
+        help_text="Добавьте ссылку на урок",
+        blank=True,
+        null=True,
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс",
+        help_text="Выберите курс",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите свою почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Номер телефона",
+        help_text="Укажите свой номер телефона",
+    )
+    city = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Город",
+        help_text="Укажите свой город",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите свой аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
